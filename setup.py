2beef9f522075e0df9436622c2c600abe872c6cb
GitHub Action <action@github.com>
2023-08-31 12:16:29 +0000

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='activemq-api-client',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     install_requires=required,
     author='Tony Schneider',
